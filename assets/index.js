9e433ba69c832badc9804df222edf664b824781c
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-07-07 13:48:05 -0500

    Add more question elements

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,19 @@
 var duration = 60; // this is the amount of seconds the countdown will start with.
 var timerDisplay = $('#timer') // this will be where the countdown is displayed in the browser
-var quizEl = document.getElementById('quiz');
+var questionTwoEl = document.getElementById('question-2');
+var questionThreeEl = document.getElementById('question-3');
+var questionFourEl = document.getElementById('question-4');
+var questionFiveEl = document.getElementById('question-5');
+var questionOneEl = document.getElementById('question-1');
 var button = $('#show-high-score');
 var begin = document.getElementById('begin');
 var quizDescription = document.getElementById('quiz-description');
 
-quizEl.setAttribute('style', 'display: none;');
+questionOneEl.setAttribute('style', 'display: none;');
+questionTwoEl.setAttribute('style', 'display: none;');
+questionThreeEl.setAttribute('style', 'display: none;');
+questionFourEl.setAttribute('style', 'display: none;');
+questionFiveEl.setAttribute('style', 'display: none;');
 
 // This starts the 60 second countdown upon clicking 'view high scores'
 // Will need to change what is clicked, but functionality works
@@ -39,4 +47,7 @@ begin.addEventListener('click', function () {
             }, 1000);
 })
 
+
+
 // REMEMBER TO PUSH TO GITHUB
+
