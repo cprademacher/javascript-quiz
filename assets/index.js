2456dbb0411aca8ce35409a160573d170b127fdc
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-07-07 12:49:27 -0500

    Added JS functionality

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,42 @@
 var duration = 60; // this is the amount of seconds the countdown will start with.
-var timerDisplay = document.getElementById('timer') // this will be where the countdown is displayed in the browser
-var quizEl = $('#quiz');
+var timerDisplay = $('#timer') // this will be where the countdown is displayed in the browser
+var quizEl = document.getElementById('quiz');
+var button = $('#show-high-score');
+var begin = document.getElementById('begin');
+var quizDescription = document.getElementById('quiz-description');
 
-var button = document.getElementById('show-high-score');
+quizEl.setAttribute('style', 'display: none;');
+
 // This starts the 60 second countdown upon clicking 'view high scores'
 // Will need to change what is clicked, but functionality works
-button.addEventListener('click', function() {
-    var countdown = setInterval(function() {
-        if (duration !== 0) {
-            timerDisplay.textContent = 'Time left: ' + duration;
-            duration--;
-        } else {
-            timerDisplay.textContent = 'Time left: 0';
-            clearInterval(countdown);
-        }
-    }, 1000);
-});
+// button.addEventListener('click', function() {
+//     var countdown = setInterval(function() {
+//         if (duration !== 0) {
+//             timerDisplay.textContent = 'Time left: ' + duration;
+//             duration--;
+//         } else {
+//             timerDisplay.textContent = 'Time left: 0';
+//             clearInterval(countdown);
+//         }
+//     }, 1000);
+// });
 
 // trying to hide the quiz until the button is pressed, not working yet
-quizEl.css('display', 'hidden');
+// quizEl.css('display', 'hidden');
+
+begin.addEventListener('click', function () {
+    quizDescription.setAttribute('style', 'display: none;');
+    quizEl.setAttribute('style', 'display: visible;');
+
+    var countdown = setInterval(function() {
+                if (duration !== 0) {
+                    timerDisplay.textContent = 'Time left: ' + duration;
+                    duration--;
+                } else {
+                    timerDisplay.textContent = 'Time left: 0';
+                    clearInterval(countdown);
+                }
+            }, 1000);
+})
+
+// REMEMBER TO PUSH TO GITHUB
