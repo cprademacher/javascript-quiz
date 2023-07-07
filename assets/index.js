fe5655092693ce15a06837a5f3b169d9fb764faa
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-07-06 22:58:14 -0500

    Add countdown functionality

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+var duration = 60; // this is the amount of seconds the countdown will start with.
+var timerDisplay = document.getElementById('timer') // this will be where the countdown is displayed in the browser
+
+var button = document.getElementById('show-high-score');
+// This starts the 60 second countdown upon clicking 'view high scores'
+// Will need to change what is clicked, but functionality works
+button.addEventListener('click', function() {
+    var countdown = setInterval(function() {
+        if (duration !== 0) {
+            timerDisplay.textContent = 'Time left: ' + duration;
+            duration--;
+        } else {
+            timerDisplay.textContent = 'Time left: 0';
+            clearInterval(countdown);
+        }
+    }, 1000);
+});
