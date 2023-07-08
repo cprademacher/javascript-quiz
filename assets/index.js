8f8e5291a1e9bf2140eeb90735104fc0c685a6e5
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-07-08 10:29:45 -0500

    Add score page and restart functionality

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,7 @@ var questionTwoEl = document.getElementById('question-2');
 var questionThreeEl = document.getElementById('question-3');
 var questionFourEl = document.getElementById('question-4');
 var questionFiveEl = document.getElementById('question-5');
-var button = $('#show-high-score');
+var button = document.getElementById('show-high-score');
 var begin = document.getElementById('begin');
 var quizDescription = document.getElementById('quiz-description');
 var correct1 = document.getElementById('correct-1');
@@ -29,13 +29,21 @@ var incorrect51 = document.getElementById('incorrect-5.1');
 var incorrect52 = document.getElementById('incorrect-5.2');
 var incorrect53 = document.getElementById('incorrect-5.3');
 var score = 0;
-// Add a variable (and correlating html element) for the quiz finished page that shows total score
+var quizOverEl = document.getElementById('quiz-over');
+var scoreEl = document.getElementById('score');
+var restart = document.getElementById('restart');
 
 questionOneEl.setAttribute('style', 'display: none;');
 questionTwoEl.setAttribute('style', 'display: none;');
 questionThreeEl.setAttribute('style', 'display: none;');
 questionFourEl.setAttribute('style', 'display: none;');
 questionFiveEl.setAttribute('style', 'display: none;');
+quizOverEl.setAttribute('style', 'display: none;');
+
+// Upon clicking the restart quiz header, the page will refresh and everything will be reset
+restart.addEventListener('click', function() {
+    location.reload();
+})
 
 // Hides the quiz until the button is pressed, once pressed question 1 will appear and the timer will start
 begin.addEventListener('click', function () {
@@ -49,11 +57,12 @@ begin.addEventListener('click', function () {
                 } else {
                     timerDisplay.textContent = 'Time left: 0';
                     clearInterval(countdown);
+                    showScore();
                 }
             }, 1000);
 })
 
-// The next four chunks of code add event listeners to the answers on question 1 and do different
+// The next chunks of code add event listeners to the answers on the questions and do different
 // things based on correct or incorrect answers.
 correct1.addEventListener('click', function () {
     questionOneEl.setAttribute('style', 'display: none;')
@@ -85,8 +94,6 @@ incorrect13.addEventListener('click', function () {
     duration = duration - 10;
 })
 
-// The next four chunks of code add event listeners to the answers on question 1 and do different
-// things based on correct or incorrect answers.
 correct2.addEventListener('click', function () {
     questionTwoEl.setAttribute('style', 'display: none;')
     questionThreeEl.setAttribute('style', 'display: visible;')
@@ -177,35 +184,52 @@ incorrect43.addEventListener('click', function () {
     duration = duration - 10;
 })
 
-correct5.addEventListener('click', function () {
-    questionFiveEl.setAttribute('style', 'display: none;')
-    // questionTwoEl.setAttribute('style', 'display: visible;')
-    // update above to show quiz over screen
-    score+= 20;
+// The function below will show the quiz-over element after the last question is
+// answered, which will show the user's score
+var showScore = function() {
+    duration = 0;
+    questionOneEl.setAttribute('style', 'display: none;');
+    questionTwoEl.setAttribute('style', 'display: none;');
+    questionThreeEl.setAttribute('style', 'display: none;');
+    questionFourEl.setAttribute('style', 'display: none;');
+    questionFiveEl.setAttribute('style', 'display: none;');
+    quizOverEl.setAttribute('style', 'display: visible');
+    scoreEl.innerHTML = 'Score: ' + score + '%';
+};
 
-    console.log(score);
+// This is supposed to clear all questions and show the quiz-over element when the
+// timer goes to 0, but it is not working for some reason.
+
+correct5.addEventListener('click', function () {
+    score+= 20;
+    questionFiveEl.setAttribute('style', 'display: none;')
+    
+    duration = 0;
+    showScore();
 })
 
 incorrect51.addEventListener('click', function () {
     questionFiveEl.setAttribute('style', 'display: none;')
-    // questionTwoEl.setAttribute('style', 'display: visible;')
-    // update above to show quiz over screen
-    duration = duration - 10;
+    
+    duration = 0;
+    showScore();
 })
 
 incorrect52.addEventListener('click', function () {
     questionFiveEl.setAttribute('style', 'display: none;')
-    // questionTwoEl.setAttribute('style', 'display: visible;')
-    // update above to show quiz over screen
-    duration = duration - 10;
+    
+    duration = 0;
+    showScore();
 })
 
 incorrect53.addEventListener('click', function () {
     questionFiveEl.setAttribute('style', 'display: none;')
-    // questionTwoEl.setAttribute('style', 'display: visible;')
-    // update above to show quiz over screen
-    duration = duration - 10;
+    
+    duration = 0;
+    showScore();
 })
 
-// REMEMBER TO PUSH TO GITHUB
+
 console.log(score);
+
+// REMEMBER TO PUSH TO GITHUB
